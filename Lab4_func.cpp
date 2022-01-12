0c15c43b8b0fc03e7b0168c2e4b09f245f44ea1f
ejik87 <59770572+ejik87@users.noreply.github.com>
2022-01-12 11:54:57 +0700

    Выполненая 4 лабораторная

---
+++ Lab4_func.cpp
@@ -0,0 +1,56 @@
+// Lab.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <math.h>
+#include <float.h>
+
+using namespace std;
+
+
+float Y(float a, float b, float c, float x)
+{
+//	float a,b,c,x;
+//	cout << "Enter a b c x:";
+//	cin >> a >> b >> c >> x;
+	return (pow(a, 3) * x - cos(x)) / (x +b * c);
+};
+
+
+float Z(float a, float b, float c, float x)
+{
+//	float a,b,c,x;
+//	cout << "Enter a b c x:";
+//	cin >> a >> b >> c >> x;
+	return -pow(10., -2) * ((b*c)/x) * pow(cos(x), 2) * sqrt(pow(a, 3) * x);
+};
+
+
+int main()
+{
+	float a,b,c,x,S,y,z;
+	cout << "Enter a b c x:";
+	cin >> a >> b >> c >> x;
+	if (x != 0 && pow(a, 3) * x >= 0 && (x +b * c) != 0)
+	{
+	y = Y(a,b,c,x);
+	z = Z(a,b,c,x);
+	S = y + z;
+
+	cout << "Y: " << y << endl;
+	cout << "Z: " << z << endl;
+	cout << "S: " << S << endl;
+	}
+	else
+	{
+		cout << "Arguments non equel null";
+	}
+
+system("pause");
+return 0;
+};
+
+
+
+
